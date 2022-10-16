cc8ebff2ec784b94f712f567a38a2a237d848d82
Utkarsh Deoli <utkarsh.deoli@gmail.com>
2022-10-16 09:55:45 +0530

    Added "Timetable Screen" and "Add Timetable Screen"

--- lib/screens/timetables_screen.dart
+++ lib/screens/timetables_screen.dart
@@ -1,21 +1,66 @@
 import 'package:flutter/material.dart';
-
+import 'package:hacktober_fest_app/screens/add_timetable_screen.dart';
+import 'package:hacktober_fest_app/widgets/timetable_screen_container.dart';
 class TimeTablesScreen extends StatelessWidget {
   static const routeName='/time-tables-screen';
+
+  const TimeTablesScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Time-Table"),
+        automaticallyImplyLeading: false,
+        leading: Padding(
+          padding: const EdgeInsets.all(9.0),
+          child: GestureDetector(
+            onTap: () {
+              Navigator.pop(context);
+            },
+            child: const Icon(
+              Icons.arrow_back_rounded,
+              size: 32,
+            ),
+          ),
+        ),
+        centerTitle: true,
+        toolbarHeight: 92,
+        backgroundColor: Colors.transparent,
+        foregroundColor: Colors.black,
+        elevation: 0,
+        shadowColor: Colors.transparent,
+        title: const Text(
+          "Timetable",
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 32,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
       ),
       floatingActionButton: FloatingActionButton(
+        foregroundColor: Colors.black,
+        backgroundColor: Color(0xffFFED8F),
         child: Icon(Icons.add),
-        onPressed: (){
-        //TODO:Add new timetable feature to be added
-      },),
-      body: Center(
-        child: Text("Time-Table Screen"),
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => const AddTimetable()),);
+          //TODO:Add new notes feature to be added
+        },
+      ),
+      body: SingleChildScrollView(
+
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: const [
+
+            TimetableScreenContainer(title: "Class Room Timetable"),
+            TimetableScreenContainer(title: "Home Timetable"),
+            TimetableScreenContainer(title: "Monthly timetable"),
+            TimetableScreenContainer(title: "Yearly Timetable"),
+          ],
+        ),
       ),
     );
   }
---
+++ lib/widgets/timetable_screen_container.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class TimetableScreenContainer extends StatelessWidget {
+  final String title;
+  const TimetableScreenContainer({super.key, required this.title});
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: (){},
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(30,15,30,15),
+        child: Container(
+          height: 80,
+          width: double.infinity,
+          //color: Color(0xff7DC9FF),
+          decoration: const BoxDecoration(
+            color: Color(0xff7DC9FF),
+            borderRadius: BorderRadius.all(Radius.circular(10)),
+          ),
+          child: Center(child: Text(title,style: const TextStyle(
+            color: Colors.white,
+            fontSize: 18,
+            fontWeight: FontWeight.w600,
+          ),)),
+        ),
+      ),
+    );
+
+  }
+}
